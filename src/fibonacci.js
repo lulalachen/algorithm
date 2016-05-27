cd01ca5411ff264ece2c27d06abb36896b69b176
lulalachen <lullalachen@gmail.com>
2016-05-27 19:14:10 +0800

    refactor(fibonacci): shorten code

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,9 +1,3 @@
-const fibonacci = (num) => {
-  if (num === 0 || num === 1) {
-    return 1
-  } else {
-    return fibonacci(num-1) + fibonacci(num-2)
-  }
-}
+const fibonacci = (num) => (num === 0 || num === 1) ? 1 : fibonacci(num-1) + fibonacci(num-2)
 
 export default fibonacci

3382fbd9e375b760880aae67caf977d5f7f4b3a9
lulalachen <lullalachen@gmail.com>
2016-05-27 17:52:54 +0800

    test(fibb): sample test

--- test/fibb.test.js
+++ test/fibb.test.js
@@ -1,3 +1,11 @@
-import fibb from '../src'
+import fibonacci from '../src'
+import expect from 'expect'
 
-console.log(fibb(10))
+describe('fibonacci', () => {
+  it('returns correct answer', () => {
+    const answers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    answers.map((ans, index) => {
+      expect(fibonacci(index)).toEqual(ans)
+    })
+  })
+})
